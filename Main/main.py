d542f702ff727aca0f8aaa4d81376747c73fcf5f
Flávio <correia.marques.94@hotmail.com>
2022-03-12 22:34:12 +0100

    [GymTracker] Second draft

--- Main/main.py
+++ Main/main.py
@@ -1,15 +1,15 @@
 import json
 
 f = open('fitnessstudios.json')
+f2 = open('adressenliste.json')
 
 # returns JSON object as
 # a dictionary
-data = json.load(f)
+data = json.load(f2)
 
 # Iterating through the json
 # list
-for i in data['features']:
-    print(i)
+print(data)
 
 # Closing file
 f.close()
