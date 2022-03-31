e4468d2f81b7ae2fb0e337a8bfd631bf56c3368c
Flávio <correia.marques.94@hotmail.com>
2022-03-31 23:20:22 +0200

    [GymTracker] After Tino's changes, new draft

---
+++ Main/address/addresses.py
@@ -0,0 +1,35 @@
+import json
+import os
+from pathlib import Path
+
+from main.address.address import Address
+
+
+# Responsible for the handling with the json file.
+# Fetches all the available addresses within it.
+class Addresses:
+
+    @staticmethod
+    def get():
+        addresses = list()
+
+        # Gets the file from the right directory, in this case var.
+        path = Path(os.getcwd())
+        file = os.path.join(path.parent.absolute().parent.absolute(), 'var', 'adressenliste.json')
+
+        with open(file, mode="r", encoding="utf-8") as json_file:
+            # returns JSON object as
+            # a dictionary
+            data = json.load(json_file)
+
+            # Iterating through the json list
+            for currentData in data:
+                street = currentData['strasse_name']
+                house_no = currentData['hausnummer']
+                house_no_extra = currentData['hausnummer_zusatz']
+                post_code = currentData['postleitzahl']
+                city = currentData['gemeinde_name']
+                town = currentData['gemeindeteil_name']
+                addresses.append(Address(street, house_no, house_no_extra, post_code, city, town))
+
+        return addresses
---
+++ main/gym/gyms.py
@@ -0,0 +1,58 @@
+import json
+import os
+from pathlib import Path
+
+from main.address.address import Address
+from main.gym.gym import Gym
+from main.utils.contact import Contact
+
+
+class Gyms:
+
+    @staticmethod
+    def get():
+        gyms = list()
+
+        # Gets the file from the right directory, in this case var.
+        path = Path(os.getcwd())
+        file = os.path.join(path.parent.absolute().parent.absolute(), 'var', 'fitnessstudios.json')
+
+        # Opening JSON file
+        with open(file, mode="r", encoding="utf-8") as json_file:
+            data = json.load(json_file)
+
+            # Iterating through the json list
+            for currentData in data['features']:
+                # Gets the properties.
+                props = currentData['properties']
+
+                # Street name, house number, postcode, part of the city, city
+                street = props['strasse_name']
+                house_no = props['hausnummer']
+                house_no_extra = props['hausnummer_zusatz']
+                post_code = props['postleitzahl']
+                city = props['gemeinde_name']
+                town = props['gemeindeteil_name']
+
+                gym_address = Address(street, house_no, house_no_extra, post_code, city, town)
+
+                # Contact possibilities
+                telephone = props['telefon_festnetz']
+                mobile = props['telefon_mobil']
+                mail = props['email']
+                website = props['website']
+
+                gym_contact = Contact(telephone, mobile, mail, website)
+
+                # Gets the gyms name.
+                name = props['bezeichnung']
+
+                # Returns the opening hours.
+                opening_hours = props['oeffnungszeiten']
+
+                # Gets the coordinates.
+                coordinates = currentData['geometry']['coordinates']
+
+                gyms.append(Gym(name, gym_address, gym_contact, opening_hours, coordinates))
+
+        return gyms
