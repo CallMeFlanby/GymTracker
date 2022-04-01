cdb264cd3cfed16b971ddfe3719b91a753a0608a
CallMeFlanby <correia.marques.94@hotmail.com>
2022-04-01 14:43:04 +0200

    [GymTracker] Search function added.

---
+++ main/utils/search.py
@@ -0,0 +1,25 @@
+from main.address.addresses import Addresses
+
+# Load all available addresses.
+addresses = Addresses.get()
+
+class Search:
+
+    # Searches a keyword and compares it with the post code, street, city and part of the city.
+    def search_address(string):
+
+        for x in addresses:
+            if string.isdigit:
+                if string in x.post_code:
+                    return x.post_code
+
+            if string in x.street.lower():
+                return x.street
+
+            if string in x.city.lower():
+                return x.city
+
+            if string in x.town_part.lower():
+                return x.town_part
+
+        return "No corresponding address."
